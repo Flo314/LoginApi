7fbf03c22cfac8b9ce07c5a7a20ce32cb46998d5
florent losada <florentlosada@MacBook-Pro-de-florent.local>
2019-12-23 17:12:12 +0100

    Activity de lancement + navigation

--- app/src/main/java/com/example/loginapi/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/loginapi/ui/home/HomeActivity.kt
@@ -2,12 +2,31 @@ package com.example.loginapi.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.loginapi.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // définir la toolbar
+        setSupportActionBar(toolbar)
+
+        // navigation -> fragment = id du navHostFragment
+        val navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupWithNavController(nav_view, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+    }
+
+    // backup navigation
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.fragment),
+            drawer_layout
+        )
     }
 }
